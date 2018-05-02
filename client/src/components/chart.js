4b82804d7fdaf4341585c7ec0ea4c0cc58c408ae
brucean52 <bruce.an52@gmail.com>
2018-05-02 15:53:13 -0700

    added chart options

--- client/src/components/chart.js
+++ client/src/components/chart.js
@@ -15,13 +15,14 @@ const styles = theme => ({
 var data = {
     labels: ['W1', 'W2', 'W3', 'W4', 'W5', 'W6', 'W7', 'W8', 'W9', 'W10'],
     series: [
-      [1, 2, 4, 8, 6, -2, -1, -4, -6, -2]
+      [1, 2, 4, 8, 6, 5, 0, 7, 6, 2]
     ]
   };
 
   var options = {
     high: 10,
-    low: -10,
+    low: 0,
+    showArea: true,
     axisX: {
       labelInterpolationFnc: function(value, index) {
         return index % 2 === 0 ? value : null;
